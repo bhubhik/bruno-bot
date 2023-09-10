c571446c54c28a8a031e9b37491c1725c717b707
bhubhik <bhubhik@gmail.com>
2023-09-10 19:36:23 +1000

    reply meme

--- index.js
+++ index.js
@@ -18,7 +18,23 @@ client.on('messageCreate', async msg => {
   if (msg.content.toLowerCase() === 'vok lagyo') {
     msg.reply('Vukk na ta!');
   }
-})
+  if(msg.content === '!meme'){
+    msg.channel.send("Lu hasnus hajur! hahaha");
+    const img = await getMeme();
+    msg.channel.send(img);
+  }
+});
+
+
+async function getMeme(){
+  try {
+    const res = await axios.get('https://meme-api.com/gimme/memes');
+    return res.data.url;
+  } catch (error) {
+    console.error('Error fetching meme:', error);
+    return 'Failed to fetch a meme. Sorry!';
+  }
+}
 
 
 // Make sure this line is the last line
